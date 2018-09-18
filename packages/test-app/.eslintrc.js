44d2b3cbdb84352ef753766aec14078419a472e4
Frank Tran <frank.tran@teamone-usa.com>
2018-09-18 12:15:09 -0700

    Move packages to monorepo structure

---
+++ packages/test-app/.eslintrc.js
@@ -0,0 +1,8 @@
+// http://eslint.org/docs/user-guide/configuring
+// You may disable eslint by exporting an empty object
+
+module.exports = {
+  extends: [
+    'eslint-config-t1-reforged'
+  ],
+};
