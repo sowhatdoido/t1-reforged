6166601f492724fa17c1ceb186153eb4f6b0870c
Frank Tran <frank.tran@teamone-usa.com>
2018-09-19 13:27:04 -0700

    Add babel/polyfill

--- packages/t1-reforged/config-overrides.js
+++ packages/t1-reforged/config-overrides.js
@@ -3,6 +3,7 @@ const rewireEslint = require('@sowhatdoido/react-app-rewire-eslint');
 
 module.exports = function override(config, env) {
   config = rewireEslint(config, env);
+  config.entry = ['@babel/polyfill'].concat(config.entry);
   config.resolve.alias['@'] = path.resolve('src/');
   return config;
 }
